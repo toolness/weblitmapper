6f48f1ba2ee4d18be9b246c891e312eed0294c46
Atul Varma <varmaa@gmail.com>
2013-11-26 15:15:00 -0500

    added tests for website.js.

---
+++ test/website.test.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var request = require('supertest');
+var should = require('should');
+
+var website = require('../').website;
+var template = require('../').template;
+
+var FakeSheet = require('./lib/fake-sheet');
+
+var ROWS = require('./sample.json');
+
+describe("website", function() {
+  var app, email, sheet;
+
+  beforeEach(function(done) {
+    email = 'foo@example.org';
+    app = express();
+    sheet = FakeSheet(ROWS);
+
+    app.use(express.bodyParser());
+    app.use(function(req, res, next) {
+      req.session = {email: email};
+      next();
+    });
+
+    template.express(app, {});
+    website.express(app, {sheet: sheet});
+    app.use(function(err, req, res, next) {
+      if (err.name && err.status)
+        return res.send(err.status, err.name);
+      throw err;
+    });
+
+    done();
+  });
+
+  it('should not provide confidential info to non-members', function(done) {
+    request(app)
+      .get('/')
+      .expect(200, function(err, res) {
+        if (err) return done(err);
+        res.text.should.not.match(/johndoe@amnh.org/);
+        done();
+      });
+  });
+
+  it('should provide confidential info to members', function(done) {
+    email = 'janedoe@amnh.org';
+    request(app)
+      .get('/')
+      .expect(/johndoe@amnh.org/)
+      .expect(200, done);
+  });
+
+  it('should not allow non-members to edit rows', function(done) {
+    request(app)
+      .post('/edit')
+      .send({id: '0'})
+      .expect(403, done);
+  });
+
+  it('should allow members to edit their rows', function(done) {
+    email = 'janedoe@amnh.org';
+    request(app)
+      .post('/edit')
+      .send({id: '0', blog: 'http://lol.org'})
+      .expect(302, function(err) {
+        if (err) return done(err);
+        sheet.rawRows[1].blog.should.eql('http://lol.org');
+        done();
+      });
+  });
+
+  it('should not allow members to edit other member\'s rows', function(done) {
+    email = 'janedoe@amnh.org';
+    request(app)
+      .post('/edit')
+      .send({id: '1'})
+      .expect(403, done);
+  });
+
+  it('should return 409 if row is invalid', function(done) {
+    request(app)
+      .post('/edit')
+      .send({id: '9999'})
+      .expect(409, done);
+  });
+});
