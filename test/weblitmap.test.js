b37a37bfc65b3b83c232f7e8e8d70908d8197a1d
Atul Varma <varmaa@gmail.com>
2014-02-06 12:30:37 -0500

    added added tests for weblitmap

---
+++ test/weblitmap.test.js
@@ -0,0 +1,15 @@
+var should = require('should');
+
+var weblitmap = require('../').weblitmap;
+
+describe('weblitmap.normalizeTags()', function() {
+  it('should remove invalid tags', function() {
+    weblitmap.normalizeTags(['']).should.eql([]);
+    weblitmap.normalizeTags(['lolol']).should.eql([]);
+  });
+
+  it('should include parent skills, competencies', function() {
+    weblitmap.normalizeTags(['ipaddresses'])
+      .should.eql(["ipaddresses", "webmechanics", "exploring"]);
+  });
+});
