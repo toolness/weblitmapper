e2027c0310d0ce7a61471ba60827164602134822
Atul Varma <varmaa@gmail.com>
2014-02-10 12:39:39 -0500

    link to user profiles on WEBMAKER_URL, not webmaker.org

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,7 @@ var gallery = new MakeGallery({
   tagPrefix: "weblit-",
   limit: 20
 }, ".make-gallery", {
+  profileBaseURL: $('meta[name="webmaker-url"]').attr('content') + '/u/',
   apiURL: $('meta[name="makeapi-url"]').attr('content'),
   hidden: ['remix-button', 'thumbnail']
 });
