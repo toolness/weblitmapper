f8693cf92f89a2464b6b280398e6cf746840ae27
Atul Varma <varmaa@gmail.com>
2014-02-10 08:17:09 -0500

    process.env.MAKEAPI_URL consulted all the time. workaround for #2.

--- test/makeapi.test.js
+++ test/makeapi.test.js
@@ -2,12 +2,39 @@ var _ = require('underscore');
 var should = require('should');
 
 var makeapi = require('../').makeapi;
+var fakeMakeapi = require('./lib/fake-makeapi-client');
 
 var BASIC_FORM = {
   url: 'http://example.org/',
   title: 'example page',
   description: 'a fine example page.'
 };
+
+beforeEach(function() {
+  process.env.MAKEAPI_URL = ':memory:';
+  fakeMakeapi.reset();
+});
+
+describe('makeapi.makeForURL', function() {
+  it('returns null when make does not exist', function(done) {
+    makeapi.makeForURL('http://lol', function(err, make) {
+      if (err) return done(err);
+      should.equal(make, null);
+      done();
+    });
+  });
+
+  it('returns make when make exists', function(done) {
+    (new fakeMakeapi()).create(BASIC_FORM, function(err) {
+      if (err) return done(err);
+      makeapi.makeForURL(BASIC_FORM.url, function(err, make) {
+        if (err) return done(err);
+        make.title.should.eql(BASIC_FORM.title);
+        done();
+      });
+    });
+  });
+});
 
 describe('makeapi.MakeForm', function() {
   it('works with tags', function() {
@@ -31,5 +58,18 @@ describe('makeapi.MakeForm', function() {
     form.updateTags(make);
 
     make.tags.should.eql(['weblit-exploring', 'foo']);
+  });
+
+  it('creates makes when needed', function(done) {
+    var form = new makeapi.MakeForm(_.clone(BASIC_FORM));
+
+    form.createOrUpdate(null, 'foo@bar.org', function(err) {
+      if (err) return done(err);
+
+      var info = fakeMakeapi.urls[BASIC_FORM.url];
+      info.title.should.eql(BASIC_FORM.title);
+
+      done();
+    });
   });
 });
