d431e68ae4b64598fb1ae9801b9feb0bd8cd938b
Atul Varma <varmaa@gmail.com>
2014-03-26 05:47:27 -0400

    added a filter field to the gallery section.

--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,17 @@
-var _ = require('underscore');
-var settings = require('./lib/public-settings');
-var MakeStream = require('./lib/make-stream');
-var InfiniteScrollStream = require('./lib/browser/infinite-scroll-stream');
-
-var makeapi = new Make({
-  apiURL: settings.MAKEAPI_URL
-});
-var makeStream = new MakeStream(makeapi, {
-  tagPrefix: settings.WEBLIT_TAG_PREFIX,
-  sortByField: 'createdAt'
-});
-var output = new InfiniteScrollStream($(".make-gallery"));
-
 $(window).load(function() {
+  var _ = require('underscore');
+  var settings = require('./lib/public-settings');
+  var MakeStream = require('./lib/make-stream');
+  var parseQuery = require('./lib/browser/parse-search-query');
+  var InfiniteScrollStream = require('./lib/browser/infinite-scroll-stream');
+
+  var query = parseQuery($("#filter-criteria").val(), {namespace: true});
+  var makeapi = new Make({apiURL: settings.MAKEAPI_URL});
+  var makeStream = new MakeStream(makeapi, _.extend(query, {
+    sortByField: 'createdAt'
+  }));
+  var output = new InfiniteScrollStream($(".make-gallery"));
+
   makeStream.pipe(output);
-}).on('scroll resize', output.onViewChanged);
+  $(window).on('scroll resize', output.onViewChanged)
+});
