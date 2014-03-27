b097a41e2d38f84133bf5d06880af848f55935a1
Atul Varma <varmaa@gmail.com>
2014-03-27 09:00:49 -0400

    prettyDate works for '1 week ago' and accepts msec since epoch.

--- test/pretty-date.test.js
+++ test/pretty-date.test.js
@@ -7,6 +7,14 @@ describe('prettyDate()', function() {
     prettyDate(new Date(Date.now() - 100)).should.eql('just now');
   });
 
+  it('should work for "1 week ago"', function() {
+    prettyDate(Date.now() - 1000*60*60*24*7).should.eql('1 week ago');
+  });
+
+  it('should work for "2 weeks ago"', function() {
+    prettyDate(Date.now() - 1000*60*60*24*7*2).should.eql('2 weeks ago');
+  });
+
   it('should work for a really long time ago', function() {
     prettyDate(new Date(0)).should.eql('on 1 January 1970');
   });
