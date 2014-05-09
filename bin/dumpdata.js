9b5e7e5ed5513df59490ab7d79ee38e534d9104a
Atul Varma <varmaa@gmail.com>
2014-05-09 17:32:03 -0400

    maybe this might fix the truncation problem w/ 'heroku run bin/dumpdata.js'.

--- bin/dumpdata.js
+++ bin/dumpdata.js
@@ -1,21 +1,44 @@
-var async = require('async');
+var Readable = require('stream').Readable;
+var util = require('util');
 var JSONStream = require('JSONStream');
 
 var db = require('../test/db');
 var allModels = require('../').model.all;
 
-var output = JSONStream.stringify();
+var jsonStream = JSONStream.stringify();
 
-output.pipe(process.stdout);
+function ModelStream(models) {
+  Readable.call(this, {objectMode: true});
+  this._models = models;
+  this._currModel = null;
+  this._currStream = null;
+}
 
-async.eachSeries(allModels(), function(model, cb) {
-  model.find({}).lean().stream().on('data', function(data) {
-    data.model = model.modelName;
-    output.write(data);
-  }).on('error', cb).on('end', cb);
-}, function(err) {
-  if (err) throw err;
+util.inherits(ModelStream, Readable);
 
-  output.end();
-  db.close();
-});
+ModelStream.prototype._read = function() {
+  if (!this._currModel) {
+    if (!this._models.length)
+      return this.push(null);
+    this._currModel = this._models.pop();
+    this._currStream = this._currModel.find({}).lean().stream();
+    this._currStream.on('error', function(err) {
+      this.emit('error', err);
+    }.bind(this));
+    this._currStream.on('data', function(data) {
+      this._currStream.pause();
+      this.push(data);
+    }.bind(this));
+    this._currStream.on('end', function() {
+      this._currModel = this._currStream = null;
+      this._read();
+    }.bind(this));
+    this._currStream.pause();
+  }
+  this._currStream.resume();
+};
+
+new ModelStream(allModels())
+  .on('end', db.close.bind(db))
+  .pipe(jsonStream)
+  .pipe(process.stdout);
